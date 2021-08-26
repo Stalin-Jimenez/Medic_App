9cbc88855f1b0b938b747e1dbe39d6b8dedbadcc
Stalin-Jimenez <stalinjimenez04@gmail.com>
2021-08-26 01:01:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/medic_app/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.medic_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    ProgressBar pb;
+    int contador =0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        prog();
+        TimerTask tarea=new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this, Principal.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer tiempo=new Timer();
+        tiempo.schedule(tarea,3000);
+
+    }
+
+    private void prog() {
+        pb=(ProgressBar)findViewById(R.id.pb);
+        final Timer tiempo=new Timer();
+        TimerTask tiempo2=new TimerTask() {
+            @Override
+            public void run() {
+                contador++;
+                pb.setProgress(contador);
+                if(contador == 100){
+                    tiempo.cancel();
+                }
+            }
+        };
+        tiempo.schedule(tiempo2,0,30);
+    }
+}
